0a05144a1fb44695db8a4200ba123668dde683f2
AFry <afry@iastate.edu>
2020-09-14 15:53:56 -0500

    MySQL Database Setup and Backend Project Setup

---
+++ Experiments/Alex Freiberg Experiments/BYTEZ_Backend/src/main/java/com/VB2/BYTEZ_Backend/BytezBackendApplication.java
@@ -0,0 +1,13 @@
+package com.VB2.BYTEZ_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytezBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytezBackendApplication.class, args);
+	}
+
+}
