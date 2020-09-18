ac289172544456d9fc10dffb6aabc4213c6057a1
AFry <afry@iastate.edu>
2020-09-18 14:55:14 -0500

    Setup and implementation for Database Entities

--- BackEnd/BYTEZ_Backend/src/main/java/com/VB2/BYTEZ_Backend/Domain/Restaurant.java
+++ BackEnd/BYTEZ_Backend/src/main/java/com/VB2/BYTEZ_Backend/Domain/Restaurant.java
@@ -3,8 +3,58 @@ package com.VB2.BYTEZ_Backend.Domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
+@Entity
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    private String RestaurantName;
+    private User owner;
+    private Double avgScore;
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRestaurantName() {
+        return RestaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        RestaurantName = restaurantName;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Double getAvgScore() {
+        return avgScore;
+    }
+
+    public void setAvgScore(Double avgScore) {
+        this.avgScore = avgScore;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- BackEnd/BYTEZ_Backend/src/main/java/com/VB2/BYTEZ_Backend/Domain/Review.java
+++ BackEnd/BYTEZ_Backend/src/main/java/com/VB2/BYTEZ_Backend/Domain/Review.java
@@ -1,6 +1,78 @@
 package com.VB2.BYTEZ_Backend.Domain;
 
-// Review Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+// Review Entity
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Restaurant restaurant;
+    private Double overallScore;
+    private Double foodQualityScore;
+    private Double serviceScore;
+    private Double cleanlinessScore;
+    private Double overallValueScore;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Double getOverallScore() {
+        return overallScore;
+    }
+
+    public void setOverallScore(Double overallScore) {
+        this.overallScore = overallScore;
+    }
+
+    public Double getFoodQualityScore() {
+        return foodQualityScore;
+    }
+
+    public void setFoodQualityScore(Double foodQualityScore) {
+        this.foodQualityScore = foodQualityScore;
+    }
+
+    public Double getServiceScore() {
+        return serviceScore;
+    }
+
+    public void setServiceScore(Double serviceScore) {
+        this.serviceScore = serviceScore;
+    }
+
+    public Double getCleanlinessScore() {
+        return cleanlinessScore;
+    }
+
+    public void setCleanlinessScore(Double cleanlinessScore) {
+        this.cleanlinessScore = cleanlinessScore;
+    }
+
+    public Double getOverallValueScore() {
+        return overallValueScore;
+    }
+
+    public void setOverallValueScore(Double overallValueScore) {
+        this.overallValueScore = (cleanlinessScore+overallValueScore+foodQualityScore+serviceScore)/4;
+    }
+    
 }
