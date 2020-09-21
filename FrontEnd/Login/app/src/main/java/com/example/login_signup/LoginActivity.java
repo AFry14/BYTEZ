3f74d2052067b0c907d908f49da5a269ecafeca0
Zach Eisele <zeisele@iastate.edu>
2020-09-21 02:53:20 -0500

    create rough login implementation

--- FrontEnd/Login/app/src/main/java/com/example/login_signup/LoginActivity.java
+++ FrontEnd/Login/app/src/main/java/com/example/login_signup/LoginActivity.java
@@ -2,31 +2,154 @@ package com.example.login_signup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity
 {
+    EditText textEmail;
+    EditText textPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        View v = findViewById(R.id.SignupB);
-        v.setOnClickListener(this);
+
+        if(SharedPrefManager.getInstance(this).isLoggedIn())
+        {
+            finish();
+            startActivity(new Intent(this, SuccessActivity.class));
+        }
+//        View v = findViewById(R.id.SignupB);
+//        v.setOnClickListener(this);
+
+        textEmail = (EditText) findViewById(R.id.editEmail);
+        textPassword = (EditText) findViewById(R.id.editPassword);
+
+        findViewById(R.id.LoginButton).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                login();
+            }
+        });
+
+        findViewById(R.id.SignupB).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(getApplicationContext(), SignUp.class));
+            }
+        });
     }
 
-
-    @Override
-    public void onClick(View view)
+    private void login()
     {
-        if(view.getId() == R.id.SignupB)
+        final String userEmail = textEmail.getText().toString();
+        final String userPassword = textPassword.getText().toString();
+
+        if(TextUtils.isEmpty(userEmail))
         {
-            Intent intent = new Intent(this, SignUp.class);
-            this.startActivity(intent);
+            textEmail.setError("Please enter an email");
+            textEmail.requestFocus();
+            return;
         }
 
+        if(TextUtils.isEmpty(userPassword))
+        {
+            textEmail.setError("Please enter your password");
+            textEmail.requestFocus();
+            return;
+        }
+
+        StringRequest strreq = new StringRequest(Request.Method.POST, URLs.URL_LOGIN, new Response.Listener<String>()
+        {
+            @Override
+            public void onResponse(String response)
+            {
+                try
+                {
+                    JSONObject obj = new JSONObject(response);
+
+                    if(!obj.getBoolean("error"))
+                    {
+                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+
+                        JSONObject JsonUser = obj.getJSONObject("user");
+
+                        User user = new User(
+                                JsonUser.getInt("id"),
+                                JsonUser.getString("username"),
+                                JsonUser.getString("email")
+                        );
+
+                        SharedPrefManager.getInstance(getApplicationContext()).loginInfo(user);
+
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), SuccessActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                catch(JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError err)
+                    {
+                        Toast.makeText(getApplicationContext(), err.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", userEmail);
+                params.put("password", userPassword);
+                return params;
+            }
+        };
+
+        SingletonVolley.getInstance(this).addToRequestQueue(strreq);
     }
+
+
+//    public void onClick(View view)
+//    {
+//        if(view.getId() == R.id.SignupB)
+//        {
+//            Intent intent = new Intent(this, SignUp.class);
+//            this.startActivity(intent);
+//        }
+//
+//    }
 }
---
+++ FrontEnd/Login/app/src/main/java/com/example/login_signup/URLs.java
@@ -0,0 +1,9 @@
+package com.example.login_signup;
+
+public class URLs
+{
+    private static final String ROOT_URL = "coms-309-vb-02.cs.iastate.edu.8080";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN = ROOT_URL + "login";
+}
