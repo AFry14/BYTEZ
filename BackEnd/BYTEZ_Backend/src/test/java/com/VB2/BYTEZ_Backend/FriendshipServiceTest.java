f9ac0f19f6748e4e08b103274ba84bb258d05bbb
AFry <afry@iastate.edu>
2020-11-01 11:02:32 -0600

    Fixed FriendshipServiceTest

--- BackEnd/BYTEZ_Backend/src/test/java/com/VB2/BYTEZ_Backend/FriendshipServiceTest.java
+++ BackEnd/BYTEZ_Backend/src/test/java/com/VB2/BYTEZ_Backend/FriendshipServiceTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -70,6 +71,7 @@ public class FriendshipServiceTest {
                     friend.setId(2L);
                     f.setSelf(self);
                     f.setFriend(friend);
+                    f.setDate(new Date());
                     fList.add(f);
                     return null;
                 });
