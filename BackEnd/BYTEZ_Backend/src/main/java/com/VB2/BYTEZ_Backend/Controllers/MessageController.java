71cb040a1fdfc6756c63702ccdf985eb4837ffbc
sethg18 <sethg18@iastate.edu>
2020-11-19 22:23:08 +0000

    Update MessageController.java

--- BackEnd/BYTEZ_Backend/src/main/java/com/VB2/BYTEZ_Backend/Controllers/MessageController.java
+++ BackEnd/BYTEZ_Backend/src/main/java/com/VB2/BYTEZ_Backend/Controllers/MessageController.java
@@ -9,6 +9,7 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.*;
